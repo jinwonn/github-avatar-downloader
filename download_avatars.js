391bc0db1e3900b8e6fd09794dc32a0e35281842
Daniel <d.t.nguyen@live.com>
2018-10-02 15:27:00 +0000

    function works to get avatars and saves them

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,84 @@
 var request = require('request');
 var GITHUB_TOKEN = require('./secret');
+var fs = require('fs');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request',
-      'Authorization': GITHUB_TOKEN
+      'User-Agent': 'request'
     }
   };
 
   request(options, function(err, res, body) {
-    parsed = JSON.parse(body)
-    // var parsed = 
-    cb(err, parsed) 
+    
+    result = JSON.parse(body)
+    cb(err, result);
+    result.forEach((result =>{
+      downloadImageByURL(result.avatar_url,result.login)
+    }))
   });
+  
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
 getRepoContributors("jquery", "jquery", function(err, result) {
-  urlresults = result.map(function(item) {
-    return item['avatar_url'];
-  });
+  if (!fs.existsSync('./avatars/')) {
+    fs.mkdirSync('./avatars/');
   console.log("Errors:", err);
-  console.log("Result:", urlresults);
+  console.log("Result:", result);
+}
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', ((err) => {
+    throw err;
+  }))
+  .on('end', function () {
+  console.log("DONE.")
+  })
+
+  // .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+  // if (!fs.existsSync('./avatars/')) {
+  //   fs.mkdirSync('./avatars/');
+  // } else {
+  .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+// }
+};
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   // ...
+//   var options = {
+//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': GITHUB_TOKEN
+//     }
+//   };
+
+//   request(options, function(err, res, body) {
+    
+//     // var parsed = 
+//     cb(err, body) 
+//   });
+// }
+
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   // urlresults = result.map(function(item) {
+//   //   return item['avatar_url'];
+//   // });
+//   if (err){
+//     throw err;
+//   }
+  // parsed = JSON.parse(result)
+//   console.log("Errors:", err);
+//   console.log("Result:", parsed);
+// });
+
+// function downloadImageByURL(url, filePath) {
+//   // ...
+// }
