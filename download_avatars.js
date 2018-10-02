e542e3e5cf4e4db8a4cac47e00d515b1e9e480ca
Daniel <d.t.nguyen@live.com>
2018-10-02 15:37:43 +0000

    added command line compatibility git status

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,14 @@
+args = process.argv.slice (2)
+account = args[0]
+repo = args[1]
 var request = require('request');
 var GITHUB_TOKEN = require('./secret');
 var fs = require('fs');
 
+if (args.length !== 2 ) {
+  console.log("please input github account and repo")
+  return
+}
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -22,7 +29,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
   
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(args[0], args[1], function(err, result) {
+  
   if (!fs.existsSync('./avatars/')) {
     fs.mkdirSync('./avatars/');
   console.log("Errors:", err);
@@ -39,46 +47,8 @@ function downloadImageByURL(url, filePath) {
   console.log("DONE.")
   })
 
-  // .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
-  // if (!fs.existsSync('./avatars/')) {
-  //   fs.mkdirSync('./avatars/');
-  // } else {
   .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
-// }
+
 };
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   // ...
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       'User-Agent': 'request',
-//       'Authorization': GITHUB_TOKEN
-//     }
-//   };
-
-//   request(options, function(err, res, body) {
-    
-//     // var parsed = 
-//     cb(err, body) 
-//   });
-// }
-
-// console.log('Welcome to the GitHub Avatar Downloader!');
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   // urlresults = result.map(function(item) {
-//   //   return item['avatar_url'];
-//   // });
-//   if (err){
-//     throw err;
-//   }
-  // parsed = JSON.parse(result)
-//   console.log("Errors:", err);
-//   console.log("Result:", parsed);
-// });
-
-// function downloadImageByURL(url, filePath) {
-//   // ...
-// }
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
